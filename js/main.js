12c472c723835462ed9b101cf216b9761aef3d4a
Kevin Ewing <notmyuwmail@gmail.com>
2015-02-21 21:50:01 -0800

    Toggle between two states

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
   APP.view = {
     _els: {
       $addMapForm: null,
+      $newMapButton: null,
     },
     init: function() {
       return this
@@ -21,17 +22,29 @@
     _bindMainButtons: function() {
       var self = this;
 
-      $('.add-map').click(function(e) {
-        // $(this) == the thing you clicked on
-        self.utils.hide($(this));
-        self.showAddMapForm();
+      this._els.$newMapButton
+        .click(function(e) {
+          // $(this) == the thing you clicked on
+          self.utils.hide($(this));
+          
+          self.showAddMapForm();
+        });
+
+      $('#new-map-submit').click(function(e) {
+        var $el = $(this);
+
+        self.utils
+          .stop(e) // For now to stop POST
+          .hide($el.closest('#new-map'))
+          .show(self._els.$newMapButton);
       });
 
       return this;
     },
     _captureElements: function() {
       this._els = {
-        $addMapForm: $('#new-map')
+        $addMapForm: $('#new-map'),
+        $newMapButton: $('.add-map')
       };
 
       return this;
@@ -39,6 +52,12 @@
   };
 
   APP.view.utils = {
+    stop: function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      return this;
+    },
     show: function($el) {
       $el
         .attr('aria-hidden', 'false')
