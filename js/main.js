a85b94583bf9094a3a8b91f6a842b54a34dcc8da
Kevin Ewing <notmyuwmail@gmail.com>
2015-02-21 20:44:41 -0800

    First form field, first JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 (function() {
-  window.App = {
+  window.APP = {
     init: function() {
+      this.view
+        .init()
+        .showAddMapForm();
+    }
+  };
+
+  APP.view = {
+    _els: {
+
+    },
+    init: function() {
+      return this
+        ._captureElements();
+    },
+    showAddMapForm: function() {
+
+    },
+    _captureElements: function() {
+      this._els = {
+
+      };
+
+      return this;
     }
   };
 
