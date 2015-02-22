c42819b22e48f0965554219080d4f780788978bf
Kevin Ewing <notmyuwmail@gmail.com>
2015-02-21 21:21:35 -0800

    Show form button

--- js/main.js
+++ js/main.js
@@ -1,33 +1,52 @@
 (function() {
   window.APP = {
     init: function() {
-      this.view
-        .init()
-        .showAddMapForm();
+      this.view.init();
     }
   };
 
   APP.view = {
     _els: {
-
+      $addMapForm: null,
     },
     init: function() {
       return this
-        ._captureElements();
+        ._captureElements()
+        ._bindMainButtons();
     },
     showAddMapForm: function() {
+      this.utils.show(this._els.$addMapForm);
+      return this;
+    },
+    _bindMainButtons: function() {
+      var self = this;
 
+      $('.add-map').click(function(e) {
+        self.showAddMapForm();
+      });
+
+      return this;
     },
     _captureElements: function() {
       this._els = {
-
+        $addMapForm: $('#new-map')
       };
 
       return this;
     }
   };
 
+  APP.view.utils = {
+    show: function($el) {
+      $el
+        .attr('aria-hidden', 'false')
+        .show();
+
+      return this;
+    }
+  }; 
+
   $(document).ready(function() {
-    App.init();
+    APP.init();
   });
 })();
